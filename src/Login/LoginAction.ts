3063220573f3b50864e0ba64eac235eb22551f89
Azhar Ahmad <Azhar.Ahmad@smartims.com>
2024-03-04 18:24:49 +0530

    Creating login screen

---
+++ src/Login/LoginAction.ts
@@ -0,0 +1,14 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { API_GET_TEST_DATA } from '../Apis/ApiEndPoints';
+import { Get } from '../Apis/AxiosInterceptor';
+import { IUser } from '../models/Dashboard/Dashboard';
+import { ICommonResponse } from '../models/Common/Common';
+import { CustomAction } from '../store/customAction';
+
+export const GetTestData = createAsyncThunk(
+  "testData",
+  async (_,{dispatch}) => {
+    const data: IUser[] = await CustomAction(Get(API_GET_TEST_DATA), dispatch, 'testData');
+    return data; 
+  }
+);
