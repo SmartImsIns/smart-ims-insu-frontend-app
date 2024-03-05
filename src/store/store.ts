45ee6399b7547fdc8e5e19b70eab64e27ae27641
Azhar Ahmad <Azhar.Ahmad@smartims.com>
2024-03-05 17:11:37 +0530

    Added authentication on routes and added header and footer

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,12 @@
 import { configureStore, combineReducers, ThunkAction, Action, AnyAction } from '@reduxjs/toolkit';
 import DashboardSlice from '../customers/Dashboard/DashboardSlice';
 import CommonSlice from '../common/CommonSlice';
+import LoginSlice from '../login/LoginSlice';
 
 const appReducer = combineReducers({
   dashboard: DashboardSlice,
   common: CommonSlice,
+  login: LoginSlice,
 });
 
 const reducerProxy = (state: any, action: AnyAction) => {
