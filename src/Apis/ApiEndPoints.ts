7cb1c85df863ba0a4bace167b959db3b033cfb2a
Srinath Munnuru <Srinath.Munnuru@smartims.com>
2024-03-06 10:29:30 +0530

    added validation for mobile number input field in login screen

--- src/Apis/ApiEndPoints.ts
+++ src/Apis/ApiEndPoints.ts
@@ -1,7 +1,4 @@
-const API_GET_TEST_DATA = 'posts';
-const API_POST_LOGIN_SUBMIT = 'login';
+const API_GET_TEST_DATA = "posts";
+const API_POST_LOGIN_SUBMIT = "login";
 
-export {
-  API_GET_TEST_DATA,
-  API_POST_LOGIN_SUBMIT
-};
+export { API_GET_TEST_DATA, API_POST_LOGIN_SUBMIT };
--- src/Apis/AxiosInterceptor.ts
+++ src/Apis/AxiosInterceptor.ts
@@ -1,28 +1,34 @@
-import axios from 'axios';
+import axios from "axios";
 
 const Api = axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL
-})
-
-Api.interceptors.request.use((config) => {
-  console.log('inside')
-  config.headers.genericKey = "someGenericValue";
-  return config;
-}, (error) => {
-  return Promise.reject(error);
+  baseURL: process.env.REACT_APP_BASE_URL,
 });
 
-Api.interceptors.response.use((response) => {
-  if(response.status === 401) {
-    alert("You are not authorized");
+Api.interceptors.request.use(
+  (config) => {
+    console.log("inside");
+    config.headers.genericKey = "someGenericValue";
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
   }
-  return response;
-}, (error) => {
-  if (error.response && error.response.data) {
-    return Promise.reject(error.response.data);
+);
+
+Api.interceptors.response.use(
+  (response) => {
+    if (response.status === 401) {
+      alert("You are not authorized");
+    }
+    return response;
+  },
+  (error) => {
+    if (error.response && error.response.data) {
+      return Promise.reject(error.response.data);
+    }
+    return Promise.reject(error.message);
   }
-  return Promise.reject(error.message);
-});
+);
 
 export const Get = Api.get;
 export const Post = Api.post;
