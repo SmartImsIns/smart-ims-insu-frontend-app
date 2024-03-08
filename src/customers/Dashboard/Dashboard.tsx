d00c1bea245f48f239e81a15d0a06dce0b86396d
Srinath Munnuru <Srinath.Munnuru@smartims.com>
2024-03-08 15:33:22 +0530

    Created tiles for polices with static data and changed login page styles

--- src/customers/Dashboard/Dashboard.tsx
+++ src/customers/Dashboard/Dashboard.tsx
@@ -1,62 +1,202 @@
-import { useEffect } from 'react';
-import { useAppDispatch, useAppSelector } from '../../store/hooks';
-import { RootState } from '../../store/store';
-import { GetTestData } from './DashboardAction';
-import { Box } from '@mui/material';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { Button, CardActionArea, CardActions } from '@mui/material';
+import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../../store/hooks";
+import { RootState } from "../../store/store";
+import { GetTestData } from "./DashboardAction";
+import { Box, IconButton } from "@mui/material";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { Button, CardActions } from "@mui/material";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+import ArrowRightAltIcon from "@mui/icons-material/ArrowRightAlt";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import MoneyIcon from "@mui/icons-material/Money";
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import DirectionsCarIcon from "@mui/icons-material/DirectionsCar";
+import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
+import DashboardStyle from "./DashboardStyle";
 
 const Dashboard = () => {
   const dispatch = useAppDispatch();
   const { testData } = useAppSelector((store: RootState) => store.dashboard);
 
-  console.log('testData', testData);
   useEffect(() => {
     dispatch(GetTestData());
   }, [dispatch]);
 
+  console.log(testData);
+
   const getPolicies = () => {
-    const element = [1, 2, 3].map((item: any, index: number) => {
+    const element = [
+      {
+        policyName: "Auto_Garage101",
+        policyCode: "12AW1145G057",
+        amount: "821",
+        tenure: "02",
+        vehicles: "02",
+        status: "active",
+        description: "Policy will auto renew in 12 days. cancel auto renewal",
+      },
+      {
+        policyName: "House_02 Policy",
+        policyCode: "12AW1145G057",
+        amount: "821",
+        tenure: "01",
+        vehicles: "01",
+        status: "active",
+        description: "Policy will auto renew in 12 days. cancel auto renewal",
+      },
+      {
+        policyName: "Home Protection",
+        policyCode: "2Aa114530g7f",
+        amount: "821",
+        tenure: "01",
+        vehicles: "01",
+        status: "active",
+        description: "Policy will auto renew in 12 days. cancel auto renewal",
+      },
+    ].map((item: any, index: number) => {
       return (
-        <Card sx={{ maxWidth: 345, padding: '1rem', margin: '1rem' }} key={`policies-${index}`}>
-          <CardActionArea>
-            <CardMedia
-              component="img"
-              height="140"
-              image=""
-              alt="green iguana"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Policy {index + 1}
-              </Typography>
-              
-            </CardContent>
-          </CardActionArea>
-          <CardActions>
-            <Button size="small" color="primary">
-              Share
+        <Card sx={DashboardStyle.card} key={item.policyName}>
+          <CardMedia component="img" height="140" image="" alt="green iguana" />
+          <CardContent sx={{ height: 200, display: "grid", gap: "10px" }}>
+            <Typography sx={{ fontSize: "1.2rem", fontWeight: "bold" }}>
+              {item.policyName}
+            </Typography>
+            <Typography
+              sx={{ fontSize: "1rem", fontWeight: "bold", color: "grey" }}
+            >
+              {item.policyCode}
+            </Typography>
+            <Box
+              sx={{
+                display: "grid",
+                alignItems: "center",
+                gridTemplateColumns: "1fr 1fr",
+              }}
+            >
+              <Box sx={{ display: "flex", alignItems: "center", gap: "5px" }}>
+                <MoneyIcon /> ${item.amount}
+              </Box>
+              <Box sx={{ display: "flex", alignItems: "center", gap: "5px" }}>
+                <AccessTimeIcon /> {item.tenure}{" "}
+                {item.tenure > 1 ? "Years" : "Year"}
+              </Box>
+            </Box>
+            <Box
+              sx={{
+                display: "grid",
+                alignItems: "center",
+                gridTemplateColumns: "1fr 1fr",
+              }}
+            >
+              <Box sx={{ display: "flex", alignItems: "center", gap: "5px" }}>
+                <DirectionsCarIcon /> {item.vehicles}
+              </Box>
+              <Box sx={{ display: "flex", alignItems: "center", gap: "5px" }}>
+                <CheckCircleOutlineIcon /> {item.status}
+              </Box>
+            </Box>
+            <Typography sx={{ fontSize: "0.8rem", color: "#666666" }}>
+              {item.description}
+            </Typography>
+          </CardContent>
+          <CardActions
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              padding: "1rem",
+            }}
+          >
+            <Button
+              sx={{
+                backgroundColor: "black",
+                color: "white",
+                fontSize: "0.7rem",
+                width: "50%",
+                borderRadius: "10px",
+                ":hover": {
+                  backgroundColor: "#000000",
+                },
+              }}
+            >
+              View Policy
             </Button>
           </CardActions>
+          <IconButton
+            sx={{ position: "absolute", bottom: "12px", right: "0px" }}
+          >
+            <MoreHorizIcon
+              sx={{
+                border: "3px solid #000000",
+                borderRadius: "5rem",
+                padding: "2px",
+                fontSize: "1rem",
+                color: "#000000",
+              }}
+            />
+          </IconButton>
         </Card>
-      )
+      );
     });
-    return element
-  }
+    return element;
+  };
 
   return (
     <Box>
       <Box>
-        <Typography component="h3">My Policies (05)</Typography>
+        <Typography>Good Morning</Typography>
+        <Typography></Typography>
       </Box>
-      <Box sx={{ display: 'inline-flex', padding: '1rem', paddingBottom: 0 }}>
-        {getPolicies()}
+      <Box
+        sx={{
+          width: "75%",
+          "@media(max-width: 780px)": {
+            width: "100%",
+          },
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <Typography sx={{ fontWeight: "bold", fontSize: "1.2rem" }}>
+            My Policies (05)
+          </Typography>
+          <Box sx={{ display: "flex" }}>
+            <IconButton>
+              <AddCircleOutlineIcon sx={{ fontSize: "2rem", color: "black" }} />
+            </IconButton>
+            <Box sx={{ display: "flex", alignItems: "center" }}>
+              <Typography
+                sx={{ fontSize: "0.8rem", textDecorationLine: "underline" }}
+              >
+                View All
+              </Typography>
+              <IconButton>
+                <ArrowRightAltIcon sx={{ fontSize: "1.5rem" }} />
+              </IconButton>
+            </Box>
+          </Box>
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            gap: "2rem",
+            "@media(max-width: 780px)": {
+              flexDirection: "column",
+            },
+          }}
+        >
+          {getPolicies()}
+        </Box>
       </Box>
     </Box>
   );
-}
+};
 
 export default Dashboard;
--- src/login/LoginStyle.ts
+++ src/login/LoginStyle.ts
@@ -1,51 +1,43 @@
 const LoginStyle = {
-  fieldSectionBox: {
-    display: 'inline-flex',
-    width: '100%',
-    margin: '1rem',
+  loginContainer: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    minHeight: "90vh",
+    "@media(max-width: 780px)": {
+      padding: "2rem",
+    },
   },
   card: {
-    maxWidth: 450,
-    margin: '10rem 24rem 10rem 26rem',
-    border: '3px solid lightgray',
-    padding: '1rem',
+    display: "grid",
+    placeItems: "center",
+    gap: "2rem",
+    width: "30%",
+    padding: "2rem",
+    boxShadow: "5",
+    "@media(max-width: 780px)": {
+      width: "100%",
+      border: "none",
+    },
   },
   heading: {
-    fontSize: '1.5rem',
-    fontWeight: 'bold',
-    textAlign: 'center',
-    marginTop: '1rem',
+    fontSize: "1.5rem",
+    fontWeight: "bold",
   },
-  labelBox: {
-    width: '30%',
-    display: 'flex',
-    alignItems: 'center',
-  },
-  fieldBox: {
-    width: '100%'
-  },
-  field: {
-    width: '90%'
-  },
-  btnBox: {
-    display: 'flex',
-    justifyContent: 'center',
-    margin: '0.5rem 1rem 1rem 1rem',
-  },
+  inputField: { width: "100%" },
   btn: {
-    width: '115px',
-    border: '1px solid',
-    backgroundColor: '#000000',
-    color: '#ffffff',
-    borderRadius: '8px',
-    marginRight: '2rem',
-    ':hover': {
-        backgroundColor: '#000000',
+    width: "115px",
+    border: "1px solid",
+    backgroundColor: "#000000",
+    color: "#ffffff",
+    borderRadius: "8px",
+    ":hover": {
+      backgroundColor: "#000000",
     },
-    ':disabled': {
-        backgroundColor: 'lightgrey'
-    }
-  }
+    ":disabled": {
+      backgroundColor: "lightgrey",
+    },
+  },
 };
 
 export default LoginStyle;
