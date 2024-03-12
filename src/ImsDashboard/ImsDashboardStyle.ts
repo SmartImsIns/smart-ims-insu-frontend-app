5dd7711f60aef5fbb8957105d66f256c42b91cab
Mohan-Villuri-Divami <sivasankar@divami.com>
2024-03-12 19:24:49 +0530

    added font faces globally

--- src/ImsDashboard/ImsDashboardStyle.ts
+++ src/ImsDashboard/ImsDashboardStyle.ts
@@ -4,10 +4,10 @@ const ImsDashboardStyle = {
     paddingRight: '30px',
     marginTop: "40px"
   },
-  claimsData:{
-  gap:'3.89px',
-  display:'flex',
-  flexDirection:'column'
+  claimsData: {
+    gap: '12px',
+    display: 'flex',
+    flexDirection: 'column'
 
   },
   welcomeCardStyles: {
@@ -49,14 +49,14 @@ const ImsDashboardStyle = {
     border: '1px solid #C9E1EC',
     boxShadow: '0px 0px 10px 0px rgba(0, 0, 0, 0.1)',
   },
-  ClaminsContent: {
+  ClaminsContentStyles: {
     display: "flex",
     flexDirection: "row",
     alignItems: "center",
     justifyContent: 'space-between',
     paddingRight: '42px',
     paddingLeft: '37.79px',
-    width:'100%'
+    width: '100%'
   },
   DashBoardCardItemStyles: {
     display: "flex",
@@ -64,55 +64,62 @@ const ImsDashboardStyle = {
     justifyContent: "center",
     alignItems: "center",
   },
+
   HeadStyle: {
-  overflow: 'hidden',
-  color: '#1D252B',
-  textOverflow: 'ellipsis',
-  fontSize: '18px',
-  fontStyle: 'normal',
-  fontWeight: "400",
-  lineHeight: '110%', // 19.8px
+    overflow: 'hidden',
+    color: '#1D252B',
+    textOverflow: 'ellipsis',
+    fontSize: '18px',
+    fontFamily: "Noto Sans",
+    fontStyle: 'normal',
+    fontWeight: "400",
+    lineHeight: '110%', // 19.8px
   },
- 
+
   ContentStyle: {
-  textOverflow: 'ellipsis',
+    textOverflow: 'ellipsis',
     color: "#1D252B",
     fontSize: "18px",
-  fontStyle: 'normal',
+    fontStyle: 'normal',
+    fontFamily: "Noto Sans",
     fontWeight: "300",
-  lineHeight: '110%', // 19.8px
+    lineHeight: '110%', // 19.8px
 
   },
   AttentionsCardTextStyle: {
     display: 'flex',
     flexDirection: 'column',
-    gap:'17.5px'
+    gap: '17.5px',
+    paddingLeft:'25.41px'
   },
   AttentionsCardBoxStyle: {
     display: 'flex',
     alignItems: "center",
   },
-  ActionButtonStyles:{
-          display: 'flex',
-          width: 155,
-          height: 40,
-          padding: '16px 12px',
-          justifyContent: 'center',
-          alignItems: 'center',
-          flexShrink: 0,
-          borderRadius: 40,
-          backgroundColor: '#3F3F3F',
-          boxShadow: '0px 6px 10px 0px rgba(63, 63, 63, 0.3)',
-          color: 'var(--Default-White, #FFF)',
-          fontWeight: 400,
-          lineHeight: 1,
-          letterSpacing: 0.5,
+  ActionButtonStyles: {
+    display: 'flex',
+    width: 155,
+    height: 40,
+    padding: '16px 12px',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexShrink: 0,
+    borderRadius: 40,
+    backgroundColor: '#3F3F3F',
+    boxShadow: '0px 6px 10px 0px rgba(63, 63, 63, 0.3)',
+    color: 'var(--Default-White, #FFF)',
+    fontWeight: 400,
+    fontFamily: "Noto Sans",
+
+    lineHeight: 1,
+    letterSpacing: 0.5,
   },
   welcomeTextStyle: {
     color: "#383D4E",
     fontSize: "20px",
     fontStyle: "normal",
     fontWeight: 400,
+    fontFamily: "Playfair Display",
     lineHeight: "110%", /*22px*/
   }
 }
