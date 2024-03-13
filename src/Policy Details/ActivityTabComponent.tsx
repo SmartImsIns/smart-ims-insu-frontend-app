69226f31ca74aaa49e8ff367066c944b98513bcd
Mohan-Villuri-Divami <sivasankar@divami.com>
2024-03-13 17:16:19 +0530

    SI-31 Developed layout for Breadcrumb, basic info and tab component

---
+++ src/Policy Details/ActivityTabComponent.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const ActivityTabComponent = (props: Props) => {
+  return (
+    <div>ActivityTabComponent</div>
+  )
+}
+
+export default ActivityTabComponent;
---
+++ src/Policy Details/CoverageComponent.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const CoverageComponent = (props: Props) => {
+  return (
+    <div>CoverageComponent</div>
+  )
+}
+
+export default CoverageComponent
