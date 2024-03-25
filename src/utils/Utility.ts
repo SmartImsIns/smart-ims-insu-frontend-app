9cbd5de70c5c05f8d36bf9aa1e922ad7f0ae10a6
Azhar Ahmad <Azhar.Ahmad@smartims.com>
2024-03-25 11:53:57 +0530

    Implementing logout functionality

--- src/utils/Utility.ts
+++ src/utils/Utility.ts
@@ -9,6 +9,12 @@ export const getCookie = (name: string) => {
   return cookieValue;
 }
 
+export const deleteCookie = (...names: any) => {
+  names.forEach((item: string) => {
+    document.cookie = `${item}= ; Expires = Thu, 01 Jan 1970 00:00:00 GMT; path=/`;
+  });
+};
+
 export const formatNumber: (number: number | undefined) => string = (number) => {
   return number?.toLocaleString("en-US", { minimumIntegerDigits: 2, useGrouping: false }) || "";
 };
