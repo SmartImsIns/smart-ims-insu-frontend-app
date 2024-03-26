8b603d0b64cac753b92cc50f52fb4819ebd03c38
manjushaparitala <manjusha.paritala@divami.com>
2024-03-26 19:14:46 +0530

    footer

--- src/ImsDashboard/Footer/FooterStyles.ts
+++ src/ImsDashboard/Footer/FooterStyles.ts
@@ -7,9 +7,6 @@ const FooterStyle = {
     justifyContent: "space-between",
     borderTop: " 1px solid #D8D8D8",
     padding: "15px, 20px, 15px, 20px",
-    
-    bottom: 0,
-    left: 0,
     "@media(max-width: 780px)": {
       height: "104px",
       display: "flex",
